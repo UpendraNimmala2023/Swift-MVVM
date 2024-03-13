7701979fb194ff17e5e1f6256d5c86eddfecd85f
UpendraNimmala2023 <135559739+UpendraNimmala2023@users.noreply.github.com>
2024-03-13 01:05:23 -0400

    Swift-MVVM Project Created
    Project Created By Upendra Nimmala

---
+++ Swift-MVVM/Swift-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-MVVM
+//
+//  Created by Upendra Nimmala on 3/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
